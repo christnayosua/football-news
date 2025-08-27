e5db31d1044bd71d1a91b7535c219a7eafc72a48
Christna Yosua Rotinsulu <christna.yosua@ui.ac.id>
2025-08-27 14:23:04 +0700

    Tutorial 0

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,7 +33,7 @@ PRODUCTION = os.getenv('PRODUCTION', 'False').lower() == 'true'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "http://christna-yosua-footballnews.pbp.cs.ui.ac.id/"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "christna-yosua-footballnews.pbp.cs.ui.ac.id"]
 
 
 # Application definition
