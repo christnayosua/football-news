c5bef1b2060421c3397d535c5f8dbb61c0f6610e
Christna Yosua Rotinsulu <christna.yosua@ui.ac.id>
2025-09-10 14:10:26 +0700

    Tutorial 2

--- football_news/settings.py
+++ football_news/settings.py
@@ -35,6 +35,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "christna-yosua-footballnews.pbp.cs.ui.ac.id"]
 
+# Tambahan entri url proyek pws
+CSRF_TRUSTED_ORIGINS = [
+    "https://christna-yosua-footballnews.pbp.cs.ui.ac.id/"
+]
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -62,7 +67,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,24 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main, create_news, show_news
+
+# import fungsi show_xml dan show_json
+from main.views import show_xml, show_json
+
+# import fungsi show_xml_by_id dan show_json_by_id
+from main.views import show_json_by_id, show_xml_by_id
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('create-news/', create_news, name='create_news'),
+    path('news/<str:id>/', show_news, name='show_news'),
+
+    # Menambahkan path url untuk mengakses fungsi show_xml dan show_json
+    path('xml/', show_xml, name='show_xml'),
+    path('json/', show_json, name='show_json'),
+
+    # Menambahkan path url untuk mengakses fungsi show_xml_by_id dan show_json_by_id
+    path('xml/<str:news_id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:news_id>/', show_json_by_id, name='show_json_by_id'),
 ]
